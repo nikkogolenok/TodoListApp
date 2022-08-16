f87bdb5ae2b50c803d15f6812ae2fc393417053c
nikita_kogolenok <nikiton17ns@gmail.com>
2022-08-16 11:15:49 +0300

    First commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Никита Коголенок on 16.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
