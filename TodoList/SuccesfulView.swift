52ff5684c1337abd935e94ef239961725c7f3954
nikita_kogolenok <nikiton17ns@gmail.com>
2022-08-19 19:43:58 +0300

    Add image succesful Create SuccesfulView

---
+++ TodoList/SuccesfulView.swift
@@ -0,0 +1,35 @@
+//
+//  SuccesfulView.swift
+//  TodoList
+//
+//  Created by Никита Коголенок on 19.08.22.
+//
+
+import SwiftUI
+
+struct SuccesfulView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Image("succesful")
+                .frame(width: 162, height: 170)
+                .padding(.init(top: 10, leading: 107, bottom: 40, trailing: 106))
+            
+            Text("Succesful!")
+                .font(.custom(FontsStyleManager.Roboto.thin, size: 32))
+                .padding(.init(top: 0, leading: 113, bottom: 10, trailing: 113))
+            
+            Text("You have succesfully change password. Please use your new passwords when logging in.")
+                .frame(width: 330, height: 57)
+                .font(.custom(FontsStyleManager.Roboto.medium, size: 16))
+                .multilineTextAlignment(.center)
+                .padding(.init(top: 0, leading: 23, bottom: 0, trailing: 22))
+        }
+    }
+}
+
+struct SuccesfulView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuccesfulView()
+    }
+}
+
